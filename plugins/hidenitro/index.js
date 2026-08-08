250321dd7940e5781760e0c959ccdf9b8740667c
tetopearr <tetopearr@users.noreply.github.com>
2026-08-08 13:47:34 +0000

    deploy: c737f3c4ef4cc2393ad888b70f0cae0e3c819245

--- plugins/hidenitro/index.js
+++ plugins/hidenitro/index.js
@@ -1,44 +1,65 @@
-(() => {
-  // plugins/hidenitro/src/index.ts
-  var { patcher, metro } = window.revenge || window.vendetta || {};
-  var unpatches = [];
-  var src_default = {
-    onLoad: () => {
-      try {
-        if (!metro || !patcher)
-          return;
-        const chatBar = metro.findByProps("RenderGiftButton");
-        if (chatBar) {
-          unpatches.push(
-            patcher.instead(chatBar, "RenderGiftButton", () => null)
-          );
-        }
-        const settings = metro.findByProps("getSettingSections");
-        if (settings) {
-          unpatches.push(
-            patcher.after(settings, "getSettingSections", (_, res) => {
-              if (!Array.isArray(res))
-                return res;
-              return res.filter((item) => {
-                const label = String(item?.title || item?.key || "").toLowerCase();
-                return !label.includes("nitro") && !label.includes("billing");
-              });
-            })
-          );
-        }
-      } catch (err) {
-        console.error("[HideNitro Error]:", err);
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// plugins/hidenitro/src/index.ts
+var src_exports = {};
+__export(src_exports, {
+  default: () => src_default
+});
+module.exports = __toCommonJS(src_exports);
+var { patcher, metro } = window.revenge || window.vendetta || {};
+var unpatches = [];
+var src_default = {
+  onLoad: () => {
+    try {
+      if (!metro || !patcher)
+        return;
+      const chatBar = metro.findByProps("RenderGiftButton");
+      if (chatBar) {
+        unpatches.push(
+          patcher.instead(chatBar, "RenderGiftButton", () => null)
+        );
       }
-    },
-    onUnload: () => {
-      unpatches.forEach((unpatch) => {
-        try {
-          if (typeof unpatch === "function")
-            unpatch();
-        } catch (e) {
-        }
-      });
-      unpatches = [];
+      const settings = metro.findByProps("getSettingSections");
+      if (settings) {
+        unpatches.push(
+          patcher.after(settings, "getSettingSections", (_, res) => {
+            if (!Array.isArray(res))
+              return res;
+            return res.filter((item) => {
+              const label = String(item?.title || item?.key || "").toLowerCase();
+              return !label.includes("nitro") && !label.includes("billing");
+            });
+          })
+        );
+      }
+    } catch (err) {
+      console.error("[HideNitro Error]:", err);
     }
-  };
-})();
+  },
+  onUnload: () => {
+    unpatches.forEach((unpatch) => {
+      try {
+        if (typeof unpatch === "function")
+          unpatch();
+      } catch (e) {
+      }
+    });
+    unpatches = [];
+  }
+};
