54ac13a90bd4d3a0994f20484f8598dc392acc35
tetopearr <joriakkerman@gmail.com>
2026-08-08 14:26:48 +0200

    p

--- plugins/hidenitro/index.js
+++ plugins/hidenitro/index.js
@@ -1,42 +1,13 @@
 (() => {
-  const revenge = window.revenge || window.vendetta;
-  const patcher = revenge?.patcher || revenge?.metro?.patcher;
-  const metro = revenge?.metro;
-
-  let unpatches = [];
-
   return {
-    onLoad: function () {
-      if (!patcher || !metro) return;
-
-      try {
-        const chatBar = metro.findByProps("RenderGiftButton");
-        if (chatBar?.RenderGiftButton) {
-          unpatches.push(patcher.instead(chatBar, "RenderGiftButton", function () { return null; }));
-        }
-
-        const settings = metro.findByProps("getSettingSections");
-        if (settings?.getSettingSections) {
-          unpatches.push(
-            patcher.after(settings, "getSettingSections", function (_, res) {
-              if (!Array.isArray(res)) return res;
-              return res.filter(function (item) {
-                const label = (item?.title || item?.key || "").toLowerCase();
-                return !label.includes("nitro") && !label.includes("billing");
-              });
-            })
-          );
-        }
-      } catch (err) {
-        console.error("[HideNitro Error]:", err);
-      }
+    onLoad: () => {
+      console.log("loaded");
     },
-
-    onUnload: function () {
-      unpatches.forEach(function (unpatch) {
-        if (typeof unpatch === "function") unpatch();
-      });
-      unpatches = [];
+    onUnload: () => {
+      console.log("unloaded");
+    }
+  };
+})();
     }
   };
 })();
